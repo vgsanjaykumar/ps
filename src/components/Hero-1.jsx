2f749847a6f3aca96f942cbb8c8f73c9edc1b017
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-25 19:09:14 +0530

    hero section set to screen height

--- src/components/Hero-1.jsx
+++ src/components/Hero-1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero1 = () => {
     return (
-        <div className="relative w-full h-[400px] md:h-[500px] lg:h-[600px] xl:h-[800px] 2xl:h-[1000px] overflow-hidden">
+        <div className="relative w-full h-[400px] md:h-screen overflow-hidden">
             <img
                 src="/award/hero-1.jpeg"
                 alt="Award Ceremony Hero Image"
