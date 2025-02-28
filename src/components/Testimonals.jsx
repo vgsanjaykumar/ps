186b240a9849fa34a4381cca39188ae4ec3063ac
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-03-01 01:40:58 +0530

    a

--- src/components/Testimonals.jsx
+++ src/components/Testimonals.jsx
@@ -23,7 +23,7 @@ const Testimonials = () => {
     autoplaySpeed: 2500,
     responsive: [
       {
-        breakpoint: 1024, 
+        breakpoint: 1024,
         settings: { slidesToShow: 3, slidesToScroll: 1 },
       },
       {
