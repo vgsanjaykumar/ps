2d75af330acffbfd78a521566d72a839974a85a6
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-03-01 02:00:06 +0530

    test

--- src/components/Testimonals.jsx
+++ src/components/Testimonals.jsx
@@ -7,8 +7,8 @@ import "slick-carousel/slick/slick-theme.css";
 const TestimonialData = [
   { id: 1, name: "Dilshad", text: "Great service!", img: "https://picsum.photos/101/101" },
   { id: 2, name: "Sabir Ali", text: "Loved the experience!", img: "https://picsum.photos/102/102" },
-  { id: 3, name: "Dipankar Kumar", text: "Highly recommend! ", img: "https://picsum.photos/104/104" },
-  { id: 4, name: "Satya Narayan", text: "Amazing support!dcrtfvgybhndctfvgybhun", img: "https://picsum.photos/103/103" },
+  { id: 3, name: "Dipankar Kumar", text: "Highly recommend!", img: "https://picsum.photos/104/104" },
+  { id: 4, name: "Satya Narayan", text: "Amazing support!", img: "https://picsum.photos/103/103" },
 ];
 
 const Testimonials = () => {
@@ -24,7 +24,7 @@ const Testimonials = () => {
     responsive: [
       {
         breakpoint: 1024, 
-        settings: { slidesToShow: 1, slidesToScroll: 1 },
+        settings: { slidesToShow: 3, slidesToScroll: 1 },
       },
       {
         breakpoint: 768,
@@ -35,18 +35,18 @@ const Testimonials = () => {
 
   return (
     <Parallax bgImage="/award/a boy and camera.webp" strength={1000} className="py-10">
-      <div className=" mx-auto px-4">
+      <div className="container mx-auto px-4">
       
         <h1 className="text-center text-4xl font-bold text-white mb-6">Testimonials</h1>
 
        
-        <div className=" h-auto p-6 rounded-lg shadow-lg">
+        <div className=" h-auto p-6  gap-6">
           <Slider {...settings}>
             {TestimonialData.map((data) => (
-              <div key={data.id} className="p-6 shadow-md rounded-lg  text-center w-full h-full">
+              <div key={data.id} className="p-6 shadow-md rounded-lg bg-white text-center">
                 <img src={data.img} alt={data.name} className="w-20 h-20 rounded-full mx-auto mb-3" />
-                <p className="text-white font-bold">{data.text}</p>
-                <h2 className="text-lg font-semibold text-white mt-2">{data.name}</h2>
+                <p className="text-gray-600">{data.text}</p>
+                <h2 className="text-lg font-semibold text-black mt-2">{data.name}</h2>
               </div>
             ))}
           </Slider>
