1183e48a98a28d32c5a6ed0c8ff4feffe7ef1db8
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-09-23 19:08:28 +0530

    home page and gallery banner animation

--- src/components/Gallery/GalleryBanner.jsx
+++ src/components/Gallery/GalleryBanner.jsx
@@ -1,20 +1,33 @@
 import weddingImage from "/award/hero-1.jpeg";
+import { motion } from "framer-motion";
 
-export default function Gallerybanner() {
-
-
+export default function Candidbanner() {
     return (
         <div className="font-sans">
+            <motion.div
+                className="relative h-[500px] md:h-[400px] lg:h-[300px] w-full rounded-b-2xl"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+            >
+                <img
+                    src={weddingImage}
+                    alt="Wedding Photography"
+                    className="w-full h-[500px] md:h-[400px] lg:h-[300px] object-cover rounded-b-2xl"
+                />
 
-            <div className="relative h-[500px] md:h-[400px] lg:h-[300px] w-full rounded-b-2xl">
-                <img src={weddingImage} alt="Wedding Photography" className="w-full  h-[500px] md:h-[400px] lg:h-[300px]  object-cover rounded-b-2xl" />
                 <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col justify-end p-4 md:p-8 text-white">
-                    <h1 className="text-2xl sm:text-3xl md:text-5xl font-semibold text-center md:text-left">Our Gallery </h1>
+                    <motion.h1
+                        className="text-2xl sm:text-3xl md:text-5xl font-semibold text-center md:text-left"
+                        initial={{ opacity: 0, y: 40 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, delay: 0.5 }}
+                    >
+                        Our Gallery
+                    </motion.h1>
                 </div>
-            </div>
-
-
-
+            </motion.div>
         </div>
     );
 }
